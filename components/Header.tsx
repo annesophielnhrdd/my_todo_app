cf0a01b3482823a49accb4b2a2056e33e2d43322
annesophielnhrdd <a.s.lienhard@gmail.com>
2024-10-03 18:22:30 +0200

    create header and fix tailwind import

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,24 @@
+import React from "react";
+
 const Header = () => {
-  return <div className="text-pink-600">Header</div>;
+  return (
+    <>
+      <header className="bg-pink-500 text-white p-4">
+        <h1 className="text-3xl font-bold">My todo app</h1>
+        <nav className="flex space-x-4">
+          <a href="#" className="hover:underline">
+            Accueil
+          </a>
+          <a href="#" className="hover:underline">
+            À propos
+          </a>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Header;
