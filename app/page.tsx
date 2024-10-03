8b1963f65933e7b4ef194c63aa486d1c545ec77e
annesophielnhrdd <a.s.lienhard@gmail.com>
2024-10-03 18:33:43 +0200

    set add task

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,22 @@
 import React from "react";
 
 import Header from "@/components/Header";
+import AddTask from "@/components/AddTask";
 import { useState } from "react";
 
 export default function Home() {
   const [task, setTask] = useState("");
 
+  const handleCreateTask = async () => {};
+
   return (
     <>
       <Header />
+      <AddTask
+        task={task}
+        setTask={setTask}
+        handleCreateTask={handleCreateTask}
+      />
     </>
   );
 }
